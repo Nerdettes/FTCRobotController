d831c57f7a026aa5da85a4543e7229396743f473
Nerdettes <95385122+Nerdettes@users.noreply.github.com>
2022-01-09 16:31:02 -0600

    Update SEDaisyDuck.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SEDaisyDuck.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SEDaisyDuck.java
@@ -114,19 +114,17 @@ public class movementTemp extends LinearOpMode {
         // Path belongs here.
         // This should be the only part that is modified once it is correct.
 
-        goStraight(-6,MAX_SPEED,MIN_SPEED,ACCEL);
-        turnACW(45);
-        goStraight(-10,MAX_SPEED,MIN_SPEED,ACCEL);
-        //movethatarm(78 whatever numbers y'all need);
-        turnCW(45);
-        strafeBuddy(12,MAX_SPEED,MIN_SPEED,ACCEL);
-        goStraight(10,MAX_SPEED,MIN_SPEED,ACCEL);
-        spinspinducky.setPower(-1);
-        sleep(10000);
-        spinspinducky.setPower(0);
-        turnCW(90);
-        strafeBuddy(4);
-        goStraight(70,MAX_SPEED,MIN_SPEED,ACCEL);
+        moveUtils.goStraight(-6,MAX_SPEED,MIN_SPEED,ACCEL);
+        moveUtils.turnACW(45);
+        moveUtils.goStraight(-10,MAX_SPEED,MIN_SPEED,ACCEL);
+        acuatorUtils.moveThatArm(30);
+        moveUtils.turnCW(45);
+        moveUtils.strafeBuddy(12,MAX_SPEED,MIN_SPEED,ACCEL);
+        moveUtils.goStraight(10,MAX_SPEED,MIN_SPEED,ACCEL);
+        acuatorUtils.spinThatDucky(false);
+        moveUtils.turnCW(90);
+        moveUtils.strafeBuddy(4);
+        moveUtils.goStraight(70,MAX_SPEED,MIN_SPEED,ACCEL);
 
         // End Modifications of path
         // -------------------------
@@ -271,15 +269,7 @@ public class movementTemp extends LinearOpMode {
 
         // Prepare motor encoders, turns off since not running to set position
         // Calculating power instead
-        LF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        LB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        RF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        RB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        LF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        LB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        RF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        RB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        resetEncoders();
 
         // Setting power to motors
         currentPower = minPower;
@@ -329,7 +319,8 @@ public class movementTemp extends LinearOpMode {
                 RB.setPower(-powerR);
             }
         }
-
+        resetEncoders();
+        headingStraight = getHeading();
     }
 
     public void strafeBuddy(float distanceMoveInches) {
